099ead4c636375b79d9983f31419b5e5c97347f9
Adam Mikulasev <adammikulas@gmail.com>
2022-10-18 18:30:02 +1100

    fix logging in test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
