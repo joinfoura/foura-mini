d982c20cbcddc2a59a5539455a0ce1955cfe0e53
Adam Mikulasev <adammikulas@gmail.com>
2022-10-18 16:06:08 +1100

    remove jade and other unnecessary lines

--- app.js
+++ app.js
@@ -9,14 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -29,8 +24,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  res.status(err.status || 500);
-  res.render('error');
+  res.sendStatus(err.status || 500);
 });
 
 module.exports = app;
